ce150f1f58ec1f4eafaabf4fb62cedf0bda75b87
daniliuskosmaches <nasarspektrreas@gmail.com>
2025-12-24 14:30:21 +0500

    the services

---
+++ DispatchService/src/test/java/org/example/dispatchservice/DispatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dispatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ TrackerService/src/test/java/org/example/trackerservice/TrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
