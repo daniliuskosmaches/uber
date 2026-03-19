9e19a5eaeb78c213c0fd97d944e9ba671ad87391
daniliuskosmaches <nasarspektrreas@gmail.com>
2026-03-19 12:51:56 +0500

    feat: merge AuthService and DispatchService into monorepo

--- AuthService/src/main/java/org/example/authservice/AuthServiceApplication.java
+++ AuthService/src/main/java/org/example/authservice/AuthServiceApplication.java
@@ -1,13 +1,13 @@
-package org.example.dispatchservice;
+package org.example.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DispatchServiceApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DispatchServiceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
--- AuthService/src/test/java/org/example/authservice/AuthServiceApplicationTests.java
+++ AuthService/src/test/java/org/example/authservice/AuthServiceApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.dispatchservice;
+package org.example.authservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DispatchServiceApplicationTests {
+class AuthServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
