c5e6eaccaba939d934c40d4c0e038fe554faa2d5
daniliuskosmaches <nasarspektrreas@gmail.com>
2026-03-20 15:11:40 +0500

    refactor: added grpc auth service

--- AuthServices/src/test/java/org/example/authservices/AuthServicesApplicationTests.java
+++ AuthServices/src/test/java/org/example/authservices/AuthServicesApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.authservice;
+package org.example.authservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthServiceApplicationTests {
+class AuthServicesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
